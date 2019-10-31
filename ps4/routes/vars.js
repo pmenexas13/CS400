d333a7aa99ec756162192773bf2c001c2943929a
Paul Menexas <pmenexas@bu.edu>
2019-10-31 13:26:20 -0400

    charizard

--- ps4/routes/vars.js
+++ ps4/routes/vars.js
@@ -13,18 +13,12 @@ router.route('/')
 
             const result = JSON.parse(body);
 
-            const poke1 = result.chain.species.name;
-
-            const poke2 = result.chain.evolves_to[0].species.name;
-
-            const poke3 = result.chain.evolves_to[0].evolves_to[0].species.name;
-
             res.render('vars',
                 {
-                    title: 'The evolution chain of ' + `${poke1}`,
-                    evol1: poke1,
-                    evol2: poke2,
-                    evol3: poke3,
+                    title: 'The evolution chain of ',
+                    evol1: result.chain.species.name,
+                    evol2: result.chain.evolves_to[0].species.name,
+                    evol3: result.chain.evolves_to[0].evolves_to[0].species.name,
                 }
             )
         })
